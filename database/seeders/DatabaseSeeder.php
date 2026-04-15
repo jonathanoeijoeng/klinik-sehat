0e77f727d94081d0bc0faeadbdc296a58ac5332f
Jonathan <jonathan.oeijoeng@gmail.com>
2026-04-15 21:35:14 +0700

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Jonathan',
-            'email' => 'jonathan.oeijoeng@gmail.com',
-        ]);
+        // User::create([
+        //     'name' => 'Jonathan',
+        //     'email' => 'jonathan.oeijoeng@gmail.com',
+        //     'password' => bcrypt('password'),
+        // ]);
 
         $this->call([
             OrganizationSeeder::class,
